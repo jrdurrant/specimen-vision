fc50a63760e2088dfc24039b71d6daad78fde06b
jrdurrant <jdurrant14@gmail.com>
2015-07-02 16:49:06 +0100

    Fixed formatting

--- segmentation.py
+++ segmentation.py
@@ -12,7 +12,7 @@ def fillContours(image, contours):
 	outline = np.zeros((h + 2, w + 2), dtype='uint8')
 	outline[1:-1, 1:-1] = image
 
-	image = 255*np.ones_like(image)
+	image = 255 * np.ones_like(image)
 	cv2.floodFill(image, outline, (0, 0), newVal=0)
 	return image
 
@@ -30,7 +30,7 @@ def largest_components(binary_image, num_components=1, output_bounding_box=False
 def grabcut_components(image, mask, num_components=1):
 	h, w, _ = image.shape
 
-	kernel = np.ones((h/100,h/100),np.uint8)
+	kernel = np.ones((h / 100, h / 100), np.uint8)
 	foreground = cv2.erode(mask, kernel, iterations=1)
 
 	foreground = largest_components(foreground, num_components)
@@ -41,22 +41,22 @@ def grabcut_components(image, mask, num_components=1):
 	mask[np.where(foreground > 0)] = cv2.GC_FGD
 	mask[np.where(background < 255)] = cv2.GC_BGD
 
-	backgroundModel = np.zeros((1,65),np.float64)
-	foregroundModel = np.zeros((1,65),np.float64)
+	backgroundModel = np.zeros((1, 65), np.float64)
+	foregroundModel = np.zeros((1, 65), np.float64)
 
 	cv2.grabCut(image, mask, rect=None, bgdModel=backgroundModel,
 				fgdModel=foregroundModel, iterCount=10,
 				mode=cv2.GC_INIT_WITH_MASK)
 
 	mask = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
-	segmented_image = image * mask[:,:,np.newaxis]
 
-	mask_holes_removed = largest_components(mask*255, num_components=1, output_bounding_box=False)
-	return segmented_image, mask_holes_removed
+	mask_holes_removed = largest_components(mask, num_components=1, output_bounding_box=False)
+	segmented_image = image * (mask[:, :, np.newaxis])
+	return segmented_image, mask_holes_removed * 255
 
 def segment_butterfly(image, approximate=True, border=10):
 	hsv_image = cv2.cvtColor(image, cv2.cv.CV_BGR2HSV)
-	mask = 255*np.greater(hsv_image[:,:,1], 100).astype('uint8')
+	mask = 255 * np.greater(hsv_image[:, :, 1], 100).astype('uint8')
 
 	butterfly_component, bounding_rect = largest_components(mask, num_components=1, output_bounding_box=True)
 	left, top, width, height = bounding_rect
@@ -68,16 +68,16 @@ def segment_butterfly(image, approximate=True, border=10):
 	image = image[crop_top:crop_bottom, crop_left:crop_right]
 
 	if not approximate:
-		mask = grabcut_components(image, mask)[1]
+		image, mask = grabcut_components(image, mask)
 
-	return mask, image
+	return image, mask
 
 def segment_image_file(file_in, folder_out):
 	image = cv2.imread(file_in)
 	segmented_image = segment_butterfly(image, approximate=False)
 
 	file_out = os.path.join(folder_out, os.path.basename(file_in))
-	cv2.imwrite(file_out, segmented_image[0])
+	cv2.imwrite(file_out, segmented_image[1])
 
 if __name__ == "__main__":
 	apply_all_images(input_folder='data/full_image/male/',
