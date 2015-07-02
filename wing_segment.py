332507926efdc0999625054048a50a34eed05325
jrdurrant <jdurrant14@gmail.com>
2015-07-02 16:50:08 +0100

    Separated image before segmenting and modified path computation for robustness

--- wing_segment.py
+++ wing_segment.py
@@ -1,27 +1,69 @@
 import numpy as np
-from skimage.graph import route_through_array
+from skimage.graph import MCP_Geometric, route_through_array
 import cv2
+import segmentation
+import timeit
 
-def wing_segmentation(mask, wing_left=0.33, wing_right=0.66, crop=0.8, distance_weight=0.5):
+class Timer(object):
+    def __enter__(self):
+        self.start = timeit.default_timer()
+    def __exit__(self, type, value, traceback):
+        self.stop = timeit.default_timer()
+        print('Elapsed time is {:.2f}s'.format(self.stop - self.start))
+
+def centre_of_mass(binary_image):
+    h, w = binary_image.shape
+    xv, yv = np.meshgrid(np.arange(0, w), np.arange(0, h))
+
+    binary_image[np.where(binary_image > 0)] = 1
+
+    x = np.mean((binary_image*xv)[np.where(binary_image > 0)])
+    y = np.mean((binary_image*yv)[np.where(binary_image > 0)])
+
+    return y, x
+
+def shortest_path(costs, start, end):
+    height, width = costs.shape
+    offsets = [[1, -1], [1, 0], [1, 1]]
+    mcp = MCP_Geometric(costs, offsets)
+    _, traceback = mcp.find_costs([start], [end])
+    indices = np.zeros((height, 2), dtype='int32')
+    indices[0, :] = end
+    for row in range(1, height):
+        y, x = indices[row - 1, :]
+        indices[row, 0] = indices[row - 1, 0] - offsets[traceback[y, x]][0]
+        indices[row, 1] = indices[row - 1, 1] - offsets[traceback[y, x]][1]
+    
+    return indices[:, 0], indices[:, 1]
+
+def wing_segmentation(mask, wing_left=0.4, wing_right=0.6, crop=0.8, distance_weight=0.5):
     mask_crop = np.copy(mask)
-    mask_crop[mask_crop == 0] = distance_weight*255
     height, width = mask_crop.shape
 
-    crop_left, crop_right = int(width * ((1 - crop) / 2)), int(width * ((1 + crop) / 2))
+    crop_left = int(width * ((1 - crop) / 2))
+    crop_right = int(width * ((1 + crop) / 2))
+
+    wing_left = int(width*wing_left - crop_left)
+    wing_right = int(width*wing_right - crop_left)
+
+    mean_y, mean_x = centre_of_mass(mask)
+    mean_x = int(mean_x - crop_left)
 
     mask_crop = mask_crop[:, crop_left:crop_right]
     crop_width = crop_right - crop_left + 1
 
-    wing_left = int(width*wing_left - crop_left)
-    indices_left, weight = route_through_array(mask_crop, (0, wing_left), (-1, wing_left))
-    indices_left = np.array(indices_left).T
-    path_crop = np.zeros_like(mask_crop)
-    path_crop[indices_left[0], indices_left[1]] = 255
+    left_cut_y, left_cut_x = shortest_path(costs=mask_crop[:, :mean_x],
+                                           start=(0, wing_left),
+                                           end=(height - 1, mean_x - 1))
 
-    wing_right = int(width*wing_right - crop_right)
-    indices_right, weight = route_through_array(mask_crop, (0, wing_right), (-1, wing_right))
-    indices_right = np.array(indices_right).T
-    path_crop[indices_right[0], indices_right[1]] = 255
+    path_crop = np.zeros_like(mask_crop)
+    path_crop[left_cut_y, left_cut_x] = 255
+
+    right_cut_y, right_cut_x = shortest_path(costs=mask_crop[:, mean_x:],
+                                             start=(0, wing_right - mean_x),
+                                             end=(height - 1, 0))
+
+    path_crop[right_cut_y, right_cut_x + mean_x] = 255
 
     path = np.zeros((height + 2, width + 2), dtype='uint8')
     path[1:-1, (crop_left + 1):(crop_right + 1)] = path_crop
@@ -35,8 +77,17 @@ def wing_segmentation(mask, wing_left=0.33, wing_right=0.66, crop=0.8, distance_
     wing_mask[wing_mask > 0] = 1
     wing_mask = wing_mask * mask
 
-    return wing_mask, (0, crop_left + np.max(indices_left[1])), (np.max(indices_right[1]), width)
+    wing_mask = segmentation.largest_components(wing_mask, num_components=2, output_bounding_box=False)
+
+    return wing_mask, (0, crop_left + np.max(left_cut_x)), (np.min(right_cut_x), width), path
 
 if __name__ == '__main__':
-    mask = cv2.imread('debug/wing/mask_small.png')[:,:,0]
-    mask2, left_wing, right_wing = wing_segmentation(mask)
+    with Timer():
+        image = cv2.imread('data/full_image/male/BMNHE_1355281.JPG')
+        image = cv2.resize(image, (0, 0), fy=0.4, fx=0.4)
+        mask, segmented_image = segmentation.segment_butterfly(image, approximate=False)
+        cv2.imwrite('debug/wing/color.png', segmented_image)
+        cv2.imwrite('debug/wing/segmented.png', mask)
+        mask2, left_wing, right_wing, path = wing_segmentation(mask)
+        cv2.imwrite('debug/wing/wing_mask.png', mask2)
+        cv2.imwrite('debug/wing/path.png', path)
