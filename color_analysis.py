2bafdd3e15afa9e229538956b72f9a6f2294fa41
jrdurrant <jdurrant14@gmail.com>
2015-07-06 12:26:31 +0100

    Changed to use Lab color space

--- color_analysis.py
+++ color_analysis.py
@@ -1,15 +1,31 @@
 import cv2
 import numpy as np
 import os
-from scipy.cluster.vq import kmeans2
+from scipy.cluster.vq import kmeans2, ClusterError
 from collections import namedtuple
 
 Color = namedtuple('Color', ('RGB', 'proportion'))
 
-def dominant_colors(data, num_colors):
-    centroids, labels = kmeans2(data, num_colors)
+def dominant_colors(image, num_colors, mask=None):
+    image = cv2.cvtColor(image / 255.0, cv2.cv.CV_BGR2Lab)
 
-    return centroids, labels
+    if mask is not None:
+        i, j = np.where(mask > 250)
+        data = image[i, j, :]
+    else:
+        height, width = image.shape[:2]
+        data = np.reshape(image, (height * width, 3))
+
+    # kmeans algorithm has inherent randomness - result will not be exactly the same 
+    # every time. Fairly consistent with >= 30 iterations
+    centroids, labels = kmeans2(data, num_colors, iter=30)
+    counts = np.histogram(labels, bins=range(0, num_colors + 1), normed=True)[0]
+
+    centroids_RGB = cv2.cvtColor(np.reshape(centroids, (centroids.shape[0], 1, 3)), cv2.cv.CV_Lab2BGR)[:, 0, :] * 255.0
+    colors = [Color(centroid, count) for centroid, count in zip(centroids_RGB, counts)]
+    colors.sort(key=lambda color: np.mean(color.RGB))
+
+    return colors
 
 if __name__ == '__main__':
     input_folder = 'data/moths_wings/'
@@ -22,28 +38,19 @@ if __name__ == '__main__':
     right_wing_front = cv2.imread(os.path.join(input_folder, 'right_wing_front_' + filename))
     right_wing_back = cv2.imread(os.path.join(input_folder, 'right_wing_back_' + filename))
 
-    i, j = np.where(left_wing_back[:, :, 0] > 250)
-    lwb_colors = color[i, j, :]
+    colors = np.concatenate((color, color), axis=0)
+    mask = np.concatenate((left_wing_front, right_wing_front), axis=0)[:, :, 0]
 
-    i, j = np.where(right_wing_back[:, :, 0] > 250)
-    rwb_colors = color[i, j, :]
+    num_colors = 5
 
-    colors = np.concatenate((lwb_colors, rwb_colors), axis=0)
+    dc = dominant_colors(colors.astype('float32'), num_colors, mask=mask)
 
-    num_colors = 4
-
-    dc, l = dominant_colors(colors.astype('float32'), num_colors)
-
-    order = np.argsort(np.mean(dc, axis=1))[::-1]
-
-    proportions = np.zeros(num_colors)
-    for i in range(0, num_colors):
-        proportions[i] = np.sum(l == order[i])
-    proportions = (num_colors * 100) * proportions / l.shape[0]
-    proportions = np.concatenate(([0], np.cumsum(proportions)))
-
-    output = np.zeros((100,num_colors * 100, 3), dtype='float32')
-    for i in range(0, num_colors):
-        output[:, proportions[i]:proportions[i+1], :] = dc[order[i], :]
+    output_width = 100 * num_colors
+    output = np.zeros((100, output_width, 3), dtype='float32')
+    left = 0
+    for color in dc:
+        right = left + int(color.proportion * output_width)
+        output[:, left:right, :] = color.RGB
+        left = right
 
     cv2.imwrite('colors.png', output)
