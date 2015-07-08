3ba7b1b0361e6ae0b3e4f2e671b9237a8371eb96
jrdurrant <jdurrant14@gmail.com>
2015-07-08 10:13:15 +0100

    Updated to output cut during wing segmentation

--- color_analysis.py
+++ color_analysis.py
@@ -3,9 +3,12 @@ import numpy as np
 import os
 from scipy.cluster.vq import kmeans2, ClusterError
 from collections import namedtuple
+import csv
+import segmentation
+from folder import apply_all_images
 
 Color = namedtuple('Color', ('RGB', 'proportion'))
-Segment = namedtuple('Segment', ('name', 'mask'))
+Segment = namedtuple('Segment', ('name', 'mask', 'num_colors'))
 
 def dominant_colors(image, num_colors, mask=None):
     image = cv2.cvtColor(image / 255.0, cv2.cv.CV_BGR2Lab)
@@ -41,28 +44,58 @@ def visualise_colors(colors, output_width, output_height):
     return output
 
 if __name__ == '__main__':
-    input_folder = 'data/moths_wings/'
-    output_folder = 'data/moths_colors/'
-    filename = 'Basiothia_charis_f_MCB002_48.5_r.png'
-    num_colors = 5
+    input_folder = 'data/all_moths/'
+    output_folder = 'data/moths_segmented/'
+
+    apply_all_images(input_folder=input_folder,
+                     output_folder=output_folder,
+                     function=segmentation.segment_image_file)
+
+    input_folder = 'data/moths_segmented/'
+    output_folder = 'output/moths_color_analysis/'
 
     images = [image_file[6:]
               for image_file
               in os.listdir(input_folder)
               if image_file.startswith('color_')]
 
-    for filename in images:
-        color = cv2.imread(os.path.join(input_folder, 'color_' + filename))
-        abdomen = cv2.imread(os.path.join(input_folder, 'abdomen_' + filename))[:, :, 0]
-        forewings = cv2.imread(os.path.join(input_folder, 'forewings_' + filename))[:, :, 0]
-        hindwings = cv2.imread(os.path.join(input_folder, 'hindwings_' + filename))[:, :, 0]
+    with open(os.path.join(output_folder,'colors.csv'), 'wb') as csvfile:
+        writer = csv.writer(csvfile)
 
-        segments = [Segment('abdomen', abdomen),
-                    Segment('forewings', forewings),
-                    Segment('hindwings', hindwings)]
+        num_colors = [10, 5]
 
-        for segment in segments:
-            dc = dominant_colors(color.astype('float32'), num_colors, mask=segment.mask)
+        headers = ['Specimen']
+        for n in num_colors:
+            headers += ['Segment', 'Mean Hue', 'Mean Saturation']
+            headers += [channel + str(n) for n in range(n) for channel in ['R', 'G', 'B', 'P']]
 
-            output = visualise_colors(dc, 100 * num_colors, 100)
-            cv2.imwrite(os.path.join(output_folder,'{}_{}'.format(segment.name, filename)), output)
+        writer.writerow(headers)
+
+        for filename in images:
+            color = cv2.imread(os.path.join(input_folder, 'color_' + filename))
+            abdomen = cv2.imread(os.path.join(input_folder, 'abdomen_' + filename))[:, :, 0]
+            wings = cv2.imread(os.path.join(input_folder, 'wings_' + filename))[:, :, 0]
+
+            HSV = cv2.cvtColor(color, cv2.cv.CV_BGR2HSV)
+
+            segments = [Segment('wings', wings, 10),
+                        Segment('abdomen', abdomen, 5)]
+
+            current_row = [os.path.splitext(filename)[0]]
+
+            for segment in segments:
+                current_row += [segment.name, 
+                                np.mean(HSV[:, :, 0][np.where(segment.mask > 128)]),
+                                np.mean(HSV[:, :, 1][np.where(segment.mask > 128)])]
+
+                dc = dominant_colors(color.astype('float32'), segment.num_colors, mask=segment.mask)
+
+                output = visualise_colors(dc, 100 * segment.num_colors, 100)
+                cv2.imwrite(os.path.join(output_folder,'{}_{}'.format(segment.name, filename)), output)
+
+                for c in dc:
+                    current_row += [c.RGB[channel] for channel in range(0, 3)[::-1]] + [c.proportion]
+
+            writer.writerow(current_row)
+
+
--- segmentation.py
+++ segmentation.py
@@ -112,7 +112,7 @@ def make_cut(costs, start, end, seed_point):
     mask = np.zeros_like(costs)
     cv2.floodFill(mask, path, seed_point, 1)
 
-    return mask
+    return mask, path
 
 def segment_wing(mask, wing_left=0.4, wing_right=0.6, crop=0.3):
     height, width = mask.shape
@@ -126,15 +126,25 @@ def segment_wing(mask, wing_left=0.4, wing_right=0.6, crop=0.3):
 
     mean_x = int(mean_x)
 
-    left_wing_mask = make_cut(costs=mask[:, crop_width:mean_x],
-                              start=(0, wing_left - crop_width),
-                              end=(height - 1, mean_x - 1 - crop_width),
-                              seed_point=(0, height - 1))
+    left_wing_mask, left_wing_path = make_cut(costs=mask[:, crop_width:mean_x],
+                                              start=(0, wing_left - crop_width),
+                                              end=(height - 1, mean_x - 1 - crop_width),
+                                              seed_point=(0, height - 1))
 
-    right_wing_mask = make_cut(costs=mask[:, mean_x : -crop_width],
-                               start=(0, wing_right - mean_x),
-                               end=(height - 1, 0),
-                               seed_point=(width - mean_x - crop_width - 1, height - 1))
+    left_wing_path = np.pad(left_wing_path[1:-1, 1:-1],
+                            pad_width=((0, 0), (crop_width, width - mean_x)),
+                            mode='constant',
+                            constant_values=0)
+
+    right_wing_mask, right_wing_path = make_cut(costs=mask[:, mean_x : -crop_width],
+                                                start=(0, wing_right - mean_x),
+                                                end=(height - 1, 0),
+                                                seed_point=(width - mean_x - crop_width - 1, height - 1))
+
+    right_wing_path = np.pad(right_wing_path[1:-1, 1:-1],
+                            pad_width=((0, 0), (mean_x, crop_width)),
+                            mode='constant',
+                            constant_values=0)
 
     wing_mask = np.hstack((left_wing_mask,right_wing_mask))
     wing_mask = mask * np.pad(wing_mask, 
@@ -146,7 +156,7 @@ def segment_wing(mask, wing_left=0.4, wing_right=0.6, crop=0.3):
                                    num_components=2, 
                                    output_bounding_box=False)
 
-    return wing_mask
+    return wing_mask, (left_wing_path, right_wing_path)
 
 def segment_image_file(file_in, folder_out):
     image = cv2.imread(file_in)
@@ -164,7 +174,7 @@ def segment_image_file(file_in, folder_out):
     # file_out = os.path.splitext(file_out)[0] + '.png'
     # cv2.imwrite(file_out, 255*segmented_mask)
 
-    wing_mask = segment_wing(segmented_mask) / 255
+    wing_mask = segment_wing(segmented_mask)[0] / 255
     file_out = os.path.join(folder_out, 'wings_' + filename)
     file_out = os.path.splitext(file_out)[0] + '.png'
     cv2.imwrite(file_out, 255*wing_mask)
