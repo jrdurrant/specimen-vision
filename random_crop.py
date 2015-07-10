e544dbfaac4dcdaf510f61a3e74eb10d975825b5
jrdurrant <jdurrant14@gmail.com>
2015-07-10 15:50:56 +0100

    Small fixes

--- random_crop.py
+++ random_crop.py
@@ -11,8 +11,6 @@ def random_crop(image, crop_width=None, crop_height=None):
 	left = np.random.randint(0, w - crop_width)
 	top = np.random.randint(0, h - crop_height)
 	output = image[top:(top + crop_height), left:(left + crop_width), :]
-	# if np.random.rand() > 0.5:
-	# 	output = output[:,::-1,:]
 	return output
 
 def crop_folder(path):
@@ -23,10 +21,6 @@ def crop_folder(path):
 		test_image = cv2.imread(path + image_name)
 		cv2.imwrite(path + image_name, random_crop(test_image, height, width))
 		cv2.imwrite(path + 'a' + image_name, random_crop(test_image, height, width))
-		# cv2.imwrite(path + 'b' + image_name, random_crop(test_image, height, width))
-		# cv2.imwrite(path + 'c' + image_name, random_crop(test_image, height, width))
 
-crop_folder('all_images_clean/cropped_wing/female/')
-# test_image = cv2.imread('male_test.jpg')
-# test_image = random_crop(test_image)
-# cv2.imwrite('crop.png', test_image)
+if __name__ == '__main__':
+	crop_folder('all_images_clean/cropped_wing/female/')
