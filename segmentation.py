1b1e0e1501a962ce5761f253d4cea5ad4fbb6a8c
James Durrant <jdurrant14@gmail.com>
2016-06-28 10:34:16 +0100

    Optimised segmentation

--- segmentation.py
+++ segmentation.py
@@ -25,67 +25,35 @@ def largest_components(binary_image, num_components=1):
 
     return filled_image, cv2.boundingRect(np.concatenate(contours))
 
-def grabcut(image, foreground, background, iterCount, mode):
-    height, width = image.shape[:2]
-    mask = cv2.GC_PR_FGD*np.ones((height, width), dtype='uint8')
-
-    mask[np.where(foreground > 0)] = cv2.GC_FGD
-    mask[np.where(background < 255)] = cv2.GC_BGD
-
-    backgroundModel = np.zeros((1, 65), np.float64)
-    foregroundModel = np.zeros((1, 65), np.float64)
-
-    cv2.grabCut(image, mask, rect=None, bgdModel=backgroundModel, fgdModel=foregroundModel, iterCount=iterCount, mode=mode)
-
-    return np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
-
-def edge_refinement(image, mask, num_components=1):
-    h, w, _ = image.shape
-    kernel_size = h / 100
-    kernel = np.ones((kernel_size, kernel_size), np.uint8)
-
-    foreground = cv2.erode(mask, kernel, iterations=1)
-    foreground = largest_components(foreground, num_components)[0]
-
-    background = cv2.dilate(mask, kernel, iterations=1)
-
-    mask = grabcut(image, foreground, background, iterCount=100, mode=cv2.GC_INIT_WITH_MASK)
-
-    mask_holes_removed = largest_components(mask, num_components=1)[0]
-
-    segmented_image = image * (mask_holes_removed[:, :, np.newaxis] / 255)
-    return segmented_image, mask_holes_removed * 255
-
 def saliency_map(image):
     hsv_image = cv2.cvtColor(image, cv2.cv.CV_BGR2HSV)
     saliency = 0.25 * hsv_image[:, :, 2] + 0.75 * hsv_image[:, :, 1]
     return saliency.astype('float32')
 
-def crop_within_limits(image, bounding_rect):
+def crop_with_border(image, bounding_rect, border):
     left, top, crop_width, crop_height = bounding_rect
     image_height, image_width = image.shape[:2]
 
-    crop_left, crop_right = max(left, 0), min(left + crop_width, image_width)
-    crop_top, crop_bottom = max(top, 0), min(top + crop_height, image_height)
+    crop_left = max(left - border, 0)
+    crop_right = min(left + crop_width + border, image_width)
+    crop_top = max(top - border, 0)
+    crop_bottom = min(top + crop_height + border, image_height)
 
     return image[crop_top:crop_bottom, crop_left:crop_right]
 
-def segment_butterfly(image, saliency_threshold=100, approximate=True, border=10):
+def segment_butterfly(image, saliency_threshold=100, border=10):
     image_height, image_width = image.shape[:2]
-    _, mask = cv2.threshold(saliency_map(image), saliency_threshold, 255, cv2.THRESH_BINARY)
 
-    component, bounding_rect = largest_components(mask, num_components=1)
+    saliency = saliency_map(image)
+    blur = cv2.GaussianBlur(saliency, (5, 5), 0).astype(np.uint8)
+    _, mask = cv2.threshold(blur, saliency_threshold, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-    left, top, width, height = bounding_rect
-    bounding_rect = (left - border, top - border, width + 2*border, height + 2*border)
+    mask, bounding_rect = largest_components(mask, num_components=1)
     
-    image = crop_within_limits(image, bounding_rect)
-    mask = crop_within_limits(mask, bounding_rect)
+    image = crop_with_border(image, bounding_rect, border)
+    mask = crop_with_border(mask, bounding_rect, border)
 
-    if not approximate:
-        image, mask = edge_refinement(image, mask)
-
-    return image, mask
+    return image, mask.astype(np.uint8)
 
 def centre_of_mass(binary_image):
     h, w = binary_image.shape
@@ -107,7 +75,7 @@ def shortest_path(costs, start, end):
 
     return indices[:, 0], indices[:, 1]
 
-def make_cut(costs, start, end, seed_point, mask_cost=10):
+def min_cost_cut(costs, start, end, seed_point, mask_cost=10):
     cut_y, cut_x = shortest_path(costs*(mask_cost - 1) + 1, start, end)
 
     path_crop = np.zeros_like(costs)
@@ -133,20 +101,20 @@ def segment_wing(mask, wing_left=0.4, wing_right=0.6, crop=0.3):
 
     mean_x = int(mean_x)
 
-    left_wing_mask, left_wing_path = make_cut(costs=mask[:, crop_width:mean_x],
-                                              start=(0, wing_left - crop_width),
-                                              end=(height - 1, mean_x - 1 - crop_width),
-                                              seed_point=(0, height - 1))
+    left_wing_mask, left_wing_path = min_cost_cut(costs=mask[:, crop_width:mean_x],
+                                                  start=(0, wing_left - crop_width),
+                                                  end=(height - 1, mean_x - 1 - crop_width),
+                                                  seed_point=(0, height - 1))
 
     left_wing_path = np.pad(left_wing_path[1:-1, 1:-1],
                             pad_width=((0, 0), (crop_width, width - mean_x)),
                             mode='constant',
                             constant_values=0)
 
-    right_wing_mask, right_wing_path = make_cut(costs=mask[:, mean_x : -crop_width],
-                                                start=(0, wing_right - mean_x),
-                                                end=(height - 1, 0),
-                                                seed_point=(width - mean_x - crop_width - 1, height - 1))
+    right_wing_mask, right_wing_path = min_cost_cut(costs=mask[:, mean_x : -crop_width],
+                                                    start=(0, wing_right - mean_x),
+                                                    end=(height - 1, 0),
+                                                    seed_point=(width - mean_x - crop_width - 1, height - 1))
 
     right_wing_path = np.pad(right_wing_path[1:-1, 1:-1],
                             pad_width=((0, 0), (mean_x, crop_width)),
@@ -165,19 +133,17 @@ def segment_wing(mask, wing_left=0.4, wing_right=0.6, crop=0.3):
 
 def segment_image_file(file_in, folder_out):
     image = cv2.imread(file_in)
-    segmented_image, segmented_mask = segment_butterfly(image, 
-                                                        saliency_threshold=64,
-                                                        approximate=False)
+    segmented_image, segmented_mask = segment_butterfly(image, saliency_threshold=64)
 
     filename = os.path.basename(file_in)
 
     file_out = os.path.join(folder_out, 'color_' + filename)
     file_out = os.path.splitext(file_out)[0] + '.png'
-    cv2.imwrite(file_out, segmented_image)
+    cv2.imwrite(file_out, segmented_image * (segmented_mask[:, :, np.newaxis] / 255))
 
-    # file_out = os.path.join(folder_out, 'full_' + filename)
-    # file_out = os.path.splitext(file_out)[0] + '.png'
-    # cv2.imwrite(file_out, 255*segmented_mask)
+    file_out = os.path.join(folder_out, 'full_' + filename)
+    file_out = os.path.splitext(file_out)[0] + '.png'
+    cv2.imwrite(file_out, segmented_mask)
 
     wing_mask = segment_wing(segmented_mask)[0] / 255
     file_out = os.path.join(folder_out, 'wings_' + filename)
