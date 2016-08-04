c20a7d18b2818dc540ff9f472abe2dc11e895165
James Durrant <jdurrant14@gmail.com>
2016-08-04 17:18:53 +0100

    Integrating appearence measure

--- vision/measurements/subspace_shape.py
+++ vision/measurements/subspace_shape.py
@@ -1,15 +1,70 @@
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
-from skimage.transform import SimilarityTransform, estimate_transform, matrix_transform
+from skimage.transform import SimilarityTransform, estimate_transform, matrix_transform, warp
 from skimage.measure import compare_ssim
 import matplotlib.pyplot as plt
+import cv2
 
 
-def plot_closest_points(image_points, edge_points, closest_edge_points):
-    plt.plot(edge_points[:, 0], edge_points[:, 1], 'r+')
+def extract_rotated_patch(image, location, rotation, patch_size):
+    """where rotation is rotation in radians clockwise from north
+    """
+    shift_y, shift_x = location
+    tf_rotate = SimilarityTransform(rotation=(-rotation))
+    tf_shift = SimilarityTransform(translation=[-shift_x, -shift_y])
+    tf_shift_inv = SimilarityTransform(translation=[patch_size[0], patch_size[1]])
+    tform = (tf_shift + (tf_rotate + tf_shift_inv))
+    rotated = warp(image, tform.inverse, output_shape=(2 * patch_size[0] + 1, 2 * patch_size[1] + 1))
+    return rotated * 255
+
+
+def find_closest_points(current_shape, shapes, images, n=15):
+    num_points = current_shape.shape[0]
+    num_images = len(images)
+
+    angles = local_angle(current_shape)
+
+    closest_points = -np.ones((num_points, 2))
+
+    template_patches = [[extract_rotated_patch(image, shape[p, [1, 0]], patch_angle - np.pi / 2, (32, 32))
+                         for p, patch_angle
+                         in enumerate(local_angle(shape))]
+                        for image, shape
+                        in zip(images, shapes)]
+
+    for p in range(num_points):
+        # print('Point {}'.format(p))
+        current_angle = angles[p]
+        current_location = current_shape[p, :]
+
+        coords = np.stack((np.linspace(-250, 250, n), np.zeros(n)), axis=1)
+        tform = SimilarityTransform(rotation=(current_angle))
+        aligned_coords = (tform(coords) + current_location).astype(np.int)
+
+        similarity = np.zeros((n, num_images))
+
+        for index, (y, x) in enumerate(aligned_coords):
+            new_patch = extract_rotated_patch(images[0], [x, y], current_angle - np.pi / 2, (32, 32))
+            for template_index in range(num_images):
+                similarity[index, template_index] = compare_ssim(template_patches[template_index][p][:, :, 1], new_patch[:, :, 1])
+            if p == 5:
+                cv2.imwrite('patch_offset_{}.png'.format(index), np.concatenate((new_patch, template_patches[0][5]), axis=1))
+        best_offset_index = np.unravel_index(np.argmax(similarity), (n, num_images))[0]
+        # print('Best image is {}'.format(np.unravel_index(np.argmax(similarity), (n, num_images))[1]))
+        closest_points[p, :] = aligned_coords[best_offset_index, :]
+    return closest_points
+
+
+def local_angle(shape):
+    n = shape.shape[0]
+    offset = shape[np.mod(np.arange(0, n) + 1, n), :] - shape[np.mod(np.arange(0, n) - 1, n), :]
+    return np.pi - np.arctan2(offset[:, 0], offset[:, 1])
+
+
+def plot_closest_points(image_points, closest_points):
     plt.plot(image_points[:, 0], image_points[:, 1], 'b')
-    for im, ed in zip(image_points, closest_edge_points):
-        plt.plot([im[0], ed[0]], [im[1], ed[1]], 'g')
+    for image, close in zip(image_points, closest_points):
+        plt.plot([image[0], close[0]], [image[1], close[1]], 'g')
     plt.show()
 
 
@@ -54,35 +109,30 @@ def update_h(sigma2, phi, y, mu, psi):
     return np.linalg.inv(partial_1) @ partial_2
 
 
-def infer(edge_image, mu, phi, sigma2):
-    edge_points = np.array(np.where(edge_image)).T
-    edge_points[:, [0, 1]] = edge_points[:, [1, 0]]
+def infer(image, images, shapes, mu, phi, sigma2):
+    scale_estimate = min(image.shape[:2]) / 2
 
-    translation_estimate = edge_image.shape[1] / 2, edge_image.shape[0] / 2
-    scale_estimate = min(edge_image.shape) / 2
-
-    edge_nn = NearestNeighbors(n_neighbors=1).fit(edge_points)
     h = np.zeros((phi.shape[1], 1))
-    psi = SimilarityTransform(scale=scale_estimate,
-                              translation=translation_estimate)
+    psi = SimilarityTransform(scale=scale_estimate)
 
-    for iteration in range(10):
+    while True:
         w = (mu + phi @ h).reshape(-1, 2)
         image_points = matrix_transform(w, psi.params)
 
-        closest_edge_point_indices = edge_nn.kneighbors(image_points)[1].flatten()
-        closest_edge_points = edge_points[closest_edge_point_indices]
+        closest_points = find_closest_points(image_points, shapes, images)
 
-        if iteration % 2 == 0:
-            plot_closest_points(image_points, edge_points, closest_edge_points)
+        # if iteration % 2 == 0:
+        #     plot_closest_points(image_points, closest_points)
 
-        psi = estimate_transform('similarity', w, closest_edge_points)
+        psi = estimate_transform('similarity', w, closest_points)
 
         image_points = matrix_transform(w, psi.params)
 
-        closest_edge_point_indices = edge_nn.kneighbors(image_points)[1].flatten()
-        closest_edge_points = edge_points[closest_edge_point_indices]
+        closest_points = find_closest_points(image_points, shapes, images)
 
-        h = update_h(sigma2, phi, closest_edge_points, mu, psi)
+        h = update_h(sigma2, phi, closest_points, mu, psi)
 
-    return image_points
+        w = (mu + phi @ h).reshape(-1, 2)
+        image_points = matrix_transform(w, psi.params)
+
+        yield image_points
--- vision/tests/scripts/example_wing_alignment.py
+++ vision/tests/scripts/example_wing_alignment.py
@@ -5,54 +5,12 @@ from vision.image_functions import threshold
 from vision.segmentation.segment import crop_by_saliency, saliency_dragonfly
 from vision.tests import get_test_image
 from vision.measurements import subspace_shape, procrustes
-from skimage.measure import find_contours, compare_ssim
+from skimage.measure import find_contours
 from skimage.transform import SimilarityTransform, warp
 import csv
 import cv2
 import matplotlib.pyplot as plt
-
-
-def find_closest_points(edge_points, angles, shapes, images, n=5):
-    num_points = edge_points.shape[0]
-    num_images = len(images)
-
-    closest_points = -np.ones((num_points, 2))
-
-    for p in range(num_points):
-        print('Point {}'.format(p))
-        current_angle = angles[p]
-        current_location = edge_points[p, :]
-
-        coords = np.stack((np.linspace(-100, 100, n), np.zeros(n)), axis=1)
-        tform = SimilarityTransform(rotation=(current_angle))
-        aligned_coords = (tform(coords) + current_location).astype(np.int)
-
-        similarity = np.zeros((n, num_images))
-
-        template_patches = [extract_rotated_patch(image, shape[p, [1, 0]], current_angle, (32, 32))
-                            for image_index, (image, shape)
-                            in enumerate(zip(images, shapes))]
-
-        for index, (y, x) in enumerate(aligned_coords):
-            new_patch = extract_rotated_patch(images[0], [x, y], current_angle, (32, 32))
-            for template_index, template_patch in enumerate(template_patches):
-                similarity[index, template_index] = compare_ssim(template_patch[:, :, 1], new_patch[:, :, 1])
-        best_offset_index = np.unravel_index(np.argmax(similarity), (n, num_images))[0]
-        print('Best image is {}'.format(np.unravel_index(np.argmax(similarity), (n, num_images))[1]))
-        closest_points[p, :] = aligned_coords[best_offset_index, :]
-    return closest_points
-
-
-def extract_rotated_patch(image, location, rotation, patch_size):
-    """where rotation is rotation in radians clockwise from north
-    """
-    shift_y, shift_x = location
-    tf_rotate = SimilarityTransform(rotation=(-rotation))
-    tf_shift = SimilarityTransform(translation=[-shift_x, -shift_y])
-    tf_shift_inv = SimilarityTransform(translation=[patch_size[0], patch_size[1]])
-    tform = (tf_shift + (tf_rotate + tf_shift_inv))
-    rotated = warp(image, tform.inverse, output_shape=(2 * patch_size[0] + 1, 2 * patch_size[1] + 1))
-    return rotated * 255
+from skimage import draw
 
 
 def patch_distance(patch_a, patch_b):
@@ -62,20 +20,14 @@ def patch_distance(patch_a, patch_b):
     return np.sum(np.power(Gy_B[:, :, 0] - Gy_A[:, :, 0], 2) + np.power(Gx_B[:, :, 1] - Gx_A[:, :, 1], 2))
 
 
-def local_angle(shape):
-    n = shape.shape[0]
-    offset = shape[np.mod(np.arange(0, n) + 1, n), :] - shape[np.mod(np.arange(0, n) - 1, n), :]
-    return np.pi - np.arctan2(offset[:, 0], offset[:, 1])
-
-
 def render_oriented_patches(image, shape, patch_size):
-    angles = local_angle(shape)
+    angles = subspace_shape.local_angle(shape)
     n = shape.shape[0]
     patch_height, patch_width = np.array(patch_size) * 2 + 1
     output_image = np.zeros((patch_height, n * patch_width, 3))
     for i in range(n):
-        output_image[:, (i * patch_width):((i + 1) * patch_width), :] = extract_rotated_patch(image, shape[i, [1, 0]], angles[i] - np.pi / 2, patch_size)
-    return output_image * 255
+        output_image[:, (i * patch_width):((i + 1) * patch_width), :] = subspace_shape.extract_rotated_patch(image, shape[i, [1, 0]], angles[i] - np.pi / 2, patch_size)
+    return output_image
 
 
 def visualize_modes(shape_model):
@@ -112,6 +64,13 @@ def visualize_patches(image, shape, size=32):
     return output_image
 
 
+def visualize_fitting(image, fitted_shape):
+    output_image = np.copy(image)
+    perimeter = draw.polygon_perimeter(fitted_shape[:, 1], fitted_shape[:, 0])
+    draw.set_color(output_image, (perimeter[0].astype(np.int), perimeter[1].astype(np.int)), [0, 0, 255])
+    cv2.imwrite('fitted.png', output_image)
+
+
 shapes = [read_shape(i) for i in range(4)]
 aligned_shapes = procrustes.generalized_procrustes(shapes)
 
@@ -121,8 +80,6 @@ images = [get_test_image('wing_area', 'cropped', '{}.png'.format(i)) for i in ra
 
 for i in range(4):
     cv2.imwrite('patches_{}.png'.format(i), render_oriented_patches(images[i], shapes[i], (32, 32)))
-
-close = find_closest_points(shapes[0], local_angle(shapes[0]), shapes, images)
 
 # saliency_map = saliency_dragonfly(image)
 # cv2.imwrite('saliency.png', saliency_map)
@@ -142,8 +99,9 @@ close = find_closest_points(shapes[0], local_angle(shapes[0]), shapes, images)
 # edges = threshold(edges)
 # cv2.imwrite('wing_edge.png', 255 * edges)
 
-fitted_shape = subspace_shape.infer(edges, *shape_model)
-fitted_shape[:, 1] = wings_image.shape[0] - fitted_shape[:, 1]
-for vertex in fitted_shape:
-    wings_image[int(vertex[1]), int(vertex[0]), :] = [0, 0, 255]
-cv2.imwrite('wings_template.png', wings_image)
+image = get_test_image('wing_area', 'cropped', '0.png')
+inference = subspace_shape.infer(image, images, shapes, *shape_model)
+for iteration in range(5):
+    fitted_shape = next(inference)
+    print('Completed {} iteration{}'.format(iteration + 1, 's' if iteration > 0 else ''))
+    visualize_fitting(image, fitted_shape)
