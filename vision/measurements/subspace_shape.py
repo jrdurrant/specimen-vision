25537d054ee611380f2169e5decc8028c72a9ee9
James Durrant <jdurrant14@gmail.com>
2016-08-04 18:29:47 +0100

    Combining menpofit with existing ASM

--- vision/measurements/subspace_shape.py
+++ vision/measurements/subspace_shape.py
@@ -1,70 +1,14 @@
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
-from skimage.transform import SimilarityTransform, estimate_transform, matrix_transform, warp
-from skimage.measure import compare_ssim
+from skimage.transform import SimilarityTransform, estimate_transform, matrix_transform
 import matplotlib.pyplot as plt
-import cv2
 
 
-def extract_rotated_patch(image, location, rotation, patch_size):
-    """where rotation is rotation in radians clockwise from north
-    """
-    shift_y, shift_x = location
-    tf_rotate = SimilarityTransform(rotation=(-rotation))
-    tf_shift = SimilarityTransform(translation=[-shift_x, -shift_y])
-    tf_shift_inv = SimilarityTransform(translation=[patch_size[0], patch_size[1]])
-    tform = (tf_shift + (tf_rotate + tf_shift_inv))
-    rotated = warp(image, tform.inverse, output_shape=(2 * patch_size[0] + 1, 2 * patch_size[1] + 1))
-    return rotated * 255
-
-
-def find_closest_points(current_shape, shapes, images, n=15):
-    num_points = current_shape.shape[0]
-    num_images = len(images)
-
-    angles = local_angle(current_shape)
-
-    closest_points = -np.ones((num_points, 2))
-
-    template_patches = [[extract_rotated_patch(image, shape[p, [1, 0]], patch_angle - np.pi / 2, (32, 32))
-                         for p, patch_angle
-                         in enumerate(local_angle(shape))]
-                        for image, shape
-                        in zip(images, shapes)]
-
-    for p in range(num_points):
-        # print('Point {}'.format(p))
-        current_angle = angles[p]
-        current_location = current_shape[p, :]
-
-        coords = np.stack((np.linspace(-250, 250, n), np.zeros(n)), axis=1)
-        tform = SimilarityTransform(rotation=(current_angle))
-        aligned_coords = (tform(coords) + current_location).astype(np.int)
-
-        similarity = np.zeros((n, num_images))
-
-        for index, (y, x) in enumerate(aligned_coords):
-            new_patch = extract_rotated_patch(images[0], [x, y], current_angle - np.pi / 2, (32, 32))
-            for template_index in range(num_images):
-                similarity[index, template_index] = compare_ssim(template_patches[template_index][p][:, :, 1], new_patch[:, :, 1])
-            if p == 5:
-                cv2.imwrite('patch_offset_{}.png'.format(index), np.concatenate((new_patch, template_patches[0][5]), axis=1))
-        best_offset_index = np.unravel_index(np.argmax(similarity), (n, num_images))[0]
-        # print('Best image is {}'.format(np.unravel_index(np.argmax(similarity), (n, num_images))[1]))
-        closest_points[p, :] = aligned_coords[best_offset_index, :]
-    return closest_points
-
-
-def local_angle(shape):
-    n = shape.shape[0]
-    offset = shape[np.mod(np.arange(0, n) + 1, n), :] - shape[np.mod(np.arange(0, n) - 1, n), :]
-    return np.pi - np.arctan2(offset[:, 0], offset[:, 1])
-
-
-def plot_closest_points(image_points, closest_points):
+def plot_closest_points(image_points, edge_points, closest_edge_points):
+    plt.plot(edge_points[:, 0], edge_points[:, 1], 'r+')
     plt.plot(image_points[:, 0], image_points[:, 1], 'b')
-    for image, close in zip(image_points, closest_points):
-        plt.plot([image[0], close[0]], [image[1], close[1]], 'g')
+    for im, ed in zip(image_points, closest_edge_points):
+        plt.plot([im[0], ed[0]], [im[1], ed[1]], 'g')
     plt.show()
 
 
@@ -84,9 +28,7 @@ def learn(points, K=1):
 
 def update_h(sigma2, phi, y, mu, psi):
     """Updates the hidden variables using updated parameters.
-
     This is an implementation of the equation:
-
 ..  math::
         \\hat{h} = (\\sigma^2 I + \\sum_{n=1}^N \\Phi_n^T A^T A \\Phi_n)^{-1} \\sum_{n=1}^N \\Phi_n^T A^T (y_n - A \\mu_n - b)
     """
@@ -109,9 +51,17 @@ def update_h(sigma2, phi, y, mu, psi):
     return np.linalg.inv(partial_1) @ partial_2
 
 
-def infer(image, images, shapes, mu, phi, sigma2):
-    scale_estimate = min(image.shape[:2]) / 2
+def find_closest_points(image_points, edge_points):
+    pass
 
+
+def infer(edge_image, mu, phi, sigma2):
+    edge_points = np.array(np.where(edge_image)).T
+    edge_points[:, [0, 1]] = edge_points[:, [1, 0]]
+
+    scale_estimate = min(edge_image.shape)
+
+    edge_nn = NearestNeighbors(n_neighbors=1).fit(edge_points)
     h = np.zeros((phi.shape[1], 1))
     psi = SimilarityTransform(scale=scale_estimate)
 
@@ -119,18 +69,21 @@ def infer(image, images, shapes, mu, phi, sigma2):
         w = (mu + phi @ h).reshape(-1, 2)
         image_points = matrix_transform(w, psi.params)
 
-        closest_points = find_closest_points(image_points, shapes, images)
+        closest_edge_point_indices = edge_nn.kneighbors(image_points)[1].flatten()
+        closest_edge_points = edge_points[closest_edge_point_indices]
 
-        # if iteration % 2 == 0:
-        #     plot_closest_points(image_points, closest_points)
+        # if iteration % 20 == 0:
+        #     plot_closest_points(image_points, edge_points, closest_edge_points)
 
-        psi = estimate_transform('similarity', w, closest_points)
+        w = mu.reshape(-1, 2)
+        psi = estimate_transform('similarity', w, closest_edge_points)
 
         image_points = matrix_transform(w, psi.params)
 
-        closest_points = find_closest_points(image_points, shapes, images)
+        closest_edge_point_indices = edge_nn.kneighbors(image_points)[1].flatten()
+        closest_edge_points = edge_points[closest_edge_point_indices]
 
-        h = update_h(sigma2, phi, closest_points, mu, psi)
+        h = update_h(sigma2, phi, closest_edge_points, mu, psi)
 
         w = (mu + phi @ h).reshape(-1, 2)
         image_points = matrix_transform(w, psi.params)
--- vision/tests/scripts/example_asm_menpo.py
+++ vision/tests/scripts/example_asm_menpo.py
@@ -1,15 +1,12 @@
 import numpy as np
 from skimage.feature import canny
-from skimage.draw import set_color
-from vision.image_functions import threshold
-from vision.segmentation.segment import crop_by_saliency, saliency_dragonfly
 from vision.tests import get_test_image
 from vision.measurements import subspace_shape, procrustes
-from skimage.measure import find_contours
 import csv
 import cv2
-import matplotlib.pyplot as plt
 from skimage import draw
+import menpo
+import menpofit
 
 
 def read_shape(index):
@@ -29,24 +26,13 @@ aligned_shapes = procrustes.generalized_procrustes(shapes)
 
 shape_model = subspace_shape.learn(aligned_shapes, K=5)
 
-mu, phi, sigma2 = shape_model
-
-# for d in range(5):
-#     for h_v in np.linspace(-2, 2, 10):
-#         h = np.zeros((5, 1))
-#         h[d] = h_v
-#         s = mu + phi @ h
-#         s = s.reshape(-1, 2)
-#         plt.plot(s[:, 0], s[:, 1])
-#     plt.show()
-
-wings_image = get_test_image('wing_area', 'cropped', '0.png')
+wings_image = get_test_image('wing_area', 'cropped', 'unlabelled', '9.png')
 cv2.imwrite('wings.png', wings_image)
 edges = canny(wings_image[:, :, 1], 2.5)
 cv2.imwrite('wing_edge.png', 255 * edges)
 
 inference = subspace_shape.infer(edges, *shape_model)
-for iteration in range(200):
+for iteration in range(50):
     fitted_shape = next(inference)
 
 output_image = np.copy(wings_image)
@@ -54,3 +40,19 @@ points = fitted_shape[:, [1, 0]]
 perimeter = draw.polygon_perimeter(points[:, 0], points[:, 1])
 draw.set_color(output_image, (perimeter[0].astype(np.int), perimeter[1].astype(np.int)), [0, 0, 255])
 cv2.imwrite('wings_template.png', output_image)
+
+training_images = menpo.io.import_images('/home/james/vision/vision/tests/test_data/wing_area/cropped/',
+                                         verbose=True)
+
+
+patch_aam = menpofit.aam.PatchAAM(training_images, group='PTS', patch_shape=(35, 35),
+                                  diagonal=150, holistic_features=menpo.feature.fast_dsift,
+                                  max_shape_components=50, max_appearance_components=150,
+                                  verbose=True)
+
+fitter = menpofit.aam.LucasKanadeAAMFitter(patch_aam, n_shape=0.9, n_appearance=0.9)
+
+image = menpo.image.Image(np.transpose(wings_image[:, :, [2, 1, 0]], (2, 0, 1)))
+result = fitter.fit_from_shape(image, menpo.shape.PointCloud(fitted_shape[:, [1, 0]]))
+
+result.view(render_initial_shape=True, figure_size=(20, 20)).save_figure('fig.png', overwrite=True)
