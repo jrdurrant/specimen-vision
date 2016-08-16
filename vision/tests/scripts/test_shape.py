3adb1a2b30ed36960aaa24c9ecc79bc5cdd70fb2
Lawrence Hudson <quicklizard@googlemail.com>
2016-08-16 14:13:39 +0100

    [#3] Prefer assert_allclose, as per numpy docs

--- vision/tests/scripts/test_shape.py
+++ vision/tests/scripts/test_shape.py
@@ -14,4 +14,4 @@ class TestProcrustes(unittest.TestCase):
         shape = normalise_shape(shape)
         normalised_shape = normalise_shape(shape)
 
-        np.testing.assert_almost_equal(shape, normalised_shape)
+        np.testing.assert_allclose(shape, normalised_shape)
